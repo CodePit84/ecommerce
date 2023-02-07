8a3c010db05c615dc4a831e7256763b41235789d
peter <codepit84@gmail.com>
2023-02-07 09:40:32 +0100

    Ajout des Fixtures

---
+++ src/DataFixtures/CategoriesFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Categories;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CategoriesFixtures extends Fixture
+{
+    public function __construct(private SluggerInterface $slugger){}
+    
+    public function load(ObjectManager $manager): void
+    {
+        $parent = $this->createCategory('Informatique', null, $manager);
+
+        $this->createCategory('Ordinateurs portables', $parent, $manager);
+        $this->createCategory('Ecran', $parent, $manager);
+        $this->createCategory('Souris', $parent, $manager);
+
+        $parent = $this->createCategory('Mode', null, $manager);
+
+        $this->createCategory('Homme', $parent, $manager);
+        $this->createCategory('Femme', $parent, $manager);
+        $this->createCategory('Enfant', $parent, $manager);
+
+        $manager->flush();
+    }
+
+    public function createCategory(string $name, Categories $parent = null, ObjectManager $manager)
+    {
+        $category = new Categories();
+        $category->setName($name);
+        $category->setSlug($this->slugger->slug($category->getName())->lower());
+        $category->setParent($parent);
+        $manager->persist($category);
+
+        return $category;
+    }
+}
